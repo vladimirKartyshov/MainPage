2e39c1d6d2d6ef3bded1a166044851eba2e91a8a
vladimirKartyshov <vovan198080"inbox.ru>
2021-01-26 21:27:52 +0300

    initional project

---
+++ app.js
@@ -0,0 +1,14 @@
+VANTA.DOTS({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xc48df7,
+    color2: 0xca20ff,
+    backgroundColor: 0x140f0f,
+    spacing: 30.00
+})
